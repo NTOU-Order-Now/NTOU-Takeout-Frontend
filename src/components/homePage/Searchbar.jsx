3c1388a08d1fed870c46ee48a9dd2a22024ef693
劉元楷 <Yuankai_@outlook.com>
2024-12-29 07:32:22 +0800

    fixed

--- src/components/homePage/Searchbar.jsx
+++ src/components/homePage/Searchbar.jsx
@@ -77,39 +77,39 @@ const Searchbar = () => {
     };
 
     return (
-        <div className="font-notoTC flex flex-col items-center w-full max-w-[800px] my-5 mx-auto box-border">
-            <div className="flex items-center justify-between w-full px-2">
-                <div
-                    className="relative text-2xl flex items-center justify-center cursor-pointer"
-                    onClick={toggleSelectionBar}
-                >
-                    <FontAwesomeIcon icon={faFilter} />
-                    {!(selectedSortBy === "" && selectedSortDir === "") && (
-                        <div className="absolute -top-[17px] right-[-4px] z-45">
-                            <FontAwesomeIcon
-                                icon={faCircle}
-                                className="w-3"
-                                style={{ color: "#ff0a0a" }}
-                            />
-                        </div>
-                    )}
-                </div>
-                <input
-                    type="text"
-                    className="flex-grow mx-2 border-b-2 border-black outline-none text-lg py-1"
-                    placeholder="想吃什麼..."
-                    id="inputKeyword"
-                    onKeyDown={handleEnter}
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)}
-                />
-                <div
-                    className="text-2xl flex items-center justify-center cursor-pointer"
-                    onClick={handleSubmit}
-                >
-                    <FontAwesomeIcon icon={faSearch} />
-                </div>
+        <div className="font-notoTC flex flex-row items-center w-full  mt-5 px-5 box-border">
+            {/*<div className="flex items-center justify-between w-full px-2">*/}
+            <div
+                className="relative text-2xl flex items-center justify-center cursor-pointer"
+                onClick={toggleSelectionBar}
+            >
+                <FontAwesomeIcon icon={faFilter} />
+                {!(selectedSortBy === "" && selectedSortDir === "") && (
+                    <div className="absolute -top-[17px] right-[-4px] z-45">
+                        <FontAwesomeIcon
+                            icon={faCircle}
+                            className="w-3"
+                            style={{ color: "#ff0a0a" }}
+                        />
+                    </div>
+                )}
             </div>
+            <input
+                type="text"
+                className="w-full mx-2 border-2 rounded-xl  border-zinc-500 outline-none text-lg py-1"
+                placeholder=" 想吃什麼..."
+                id="inputKeyword"
+                onKeyDown={handleEnter}
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <div
+                className="text-2xl flex items-center justify-center cursor-pointer"
+                onClick={handleSubmit}
+            >
+                <FontAwesomeIcon icon={faSearch} />
+            </div>
+            {/*</div>*/}
 
             {showSelectionBar && <SearchSelectionBar></SearchSelectionBar>}
         </div>
