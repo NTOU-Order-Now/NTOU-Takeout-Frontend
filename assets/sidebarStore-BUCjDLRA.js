58801da35aab06c47b5b084613f68526f1d35c11
Yuankai619 <Yuankai619@users.noreply.github.com>
2025-01-02 21:25:10 +0000

    deploy: 9f33b659a6758a616176ac62831196ae6ccd0493

---
+++ assets/sidebarStore-BUCjDLRA.js
@@ -0,0 +1,1 @@
+import{g as s}from"./index-DZfytaCp.js";const a=s(e=>({title:"",setTitle:i=>e({title:i}),isOpen:!1,toggleSidebar:()=>e(i=>({isOpen:!i.isOpen})),closeSidebar:()=>e({isOpen:!1})}));export{a as u};
